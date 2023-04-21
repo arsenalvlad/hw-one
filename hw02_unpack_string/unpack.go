8bb5e7f27d6fae52dcec8e9243484cff3053fd49
Usov Vlad <vladislav.usov@moex.com>
2023-04-21 09:34:49 +0300

    rework HW2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,73 +2,75 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(str string) (string, error) { //nolint: gocyclo,gocognit
+func Unpack(str string) (string, error) { //nolint: gocognit
 	var result strings.Builder
 	number := "0123456789"
 
-	for i, k := range str {
-		if i == 0 && strings.Contains(number, string(k)) {
+	reg1 := regexp.MustCompile(`\\[0-9]{2}`)
+
+	reg2 := regexp.MustCompile(`[\\]{1}[0-9]{1}`)
+
+	reg3 := regexp.MustCompile(`[\\]{2}[0-9]{1}`)
+
+	reg4 := regexp.MustCompile(`[\\]{3}[0-9]{1}`)
+
+	for i := 0; i < len(str); i++ {
+		switch {
+		case i+1 != len(str) && strings.Contains(number, string(str[i])):
 			return "", ErrInvalidString
-		}
-		if strings.Contains(number, string(k)) { //nolint: nestif
-			num, err := strconv.Atoi(string(k))
+		case i+1 < len(str) && string(str[i]) == `\`:
+			if matched := reg1.MatchString(str); matched {
+				l, err := strconv.Atoi(string(str[i+2]))
+				if err != nil {
+					return "", err
+				}
+
+				result.WriteString(strings.Repeat(string(str[i+1]), l))
+				i += 2
+				continue
+			}
+
+			if matched := reg2.MatchString(str); matched { //nolint: nestif
+				if matched := reg3.MatchString(str); matched {
+					if matched := reg4.MatchString(str); matched {
+						result.WriteString(str[i+2 : i+4])
+						i += 3
+						continue
+					}
+					l, err := strconv.Atoi(string(str[i+2]))
+					if err != nil {
+						return "", err
+					}
+
+					result.WriteString(strings.Repeat(string(str[i+1]), l))
+					i += 2
+					continue
+				}
+
+				result.WriteString(string(str[i+1]))
+				i++
+				continue
+			}
+		case i+1 < len(str) && strings.Contains(number, string(str[i+1])):
+			l, err := strconv.Atoi(string(str[i+1]))
 			if err != nil {
 				return "", err
 			}
 
-			if i != 1 && i+1 == len(str) && string(str[i-1]) == `\` {
-				if i != 1 && string(str[i-2]) == `\` {
-					if i != 2 && string(str[i-3]) == `\` {
-						a := result.String()
-						return a[0:4] + string(k), nil
-					}
-					result.WriteString(strings.Repeat(string(str[i-1]), num-1))
-					continue
-				}
-				result.WriteString(string(k))
-				continue
-			}
-			if i+1 < len(str) && string(str[i-1]) == `\` && !strings.Contains(number, string(str[i+1])) {
-				result.WriteString(string(k))
-				continue
-			}
-			if i+1 < len(str) && string(str[i-1]) == `\` && strings.Contains(number, string(str[i+1])) {
-				continue
-			}
-			if i > 1 && string(str[i-2]) == `\` && !strings.Contains(number, string(str[i-1])) {
-				result.WriteString(strings.Repeat(string(str[i-2]+str[i-1]), num))
-				continue
-			}
-			if i > 1 && string(str[i-2]) == `\` && strings.Contains(number, string(str[i-1])) {
-				result.WriteString(strings.Repeat(string(str[i-1]), num))
-				continue
-			}
-			result.WriteString(strings.Repeat(string(str[i-1]), num))
-
-			if strings.Contains(number, string(str[i-1])) {
-				if i > 2 && string(str[i-2]) != `\` {
-					return "", ErrInvalidString
-				}
-				if i > 2 && string(str[i-2]) == `\` {
-					continue
-				}
-
-				return "", ErrInvalidString
+			if l < 10 {
+				result.WriteString(strings.Repeat(string(str[i]), l))
 			}
 
-			continue
-		}
-
-		if i+1 < len(str) && !strings.Contains(number, string(str[i+1])) {
-			result.WriteString(string(k))
-		} else if i+1 == len(str) {
-			result.WriteString(string(k))
+			i++
+		default:
+			result.WriteString(string(str[i]))
 		}
 	}
 

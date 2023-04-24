1fc41fd6918aae6b95baec63226897be23b4554b
Usov Vlad <vladislav.usov@moex.com>
2023-04-24 08:57:13 +0300

    hw 3 Ready

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,59 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type resStruct struct {
+	key   string
+	value int64
+}
+
+func Top10(str string) []string {
+	res := make([]string, 0, 10)
+	strMap := make(map[string]int64)
+	reg1 := regexp.MustCompile(`([a-zA-Z]+[,.!'?:])*`)
+	symbol := ",.!'?:"
+
+	strSlice := strings.Fields(strings.ToLower(str))
+
+	for _, k := range strSlice {
+		if k == "-" {
+			continue
+		}
+		if reg1.MatchString(k) {
+			for _, l := range symbol {
+				k = strings.ReplaceAll(k, string(l), "")
+			}
+		}
+		if strMap[k] == 0 {
+			strMap[k] = 1
+		} else {
+			strMap[k]++
+		}
+	}
+
+	resSort := make([]resStruct, 0, len(strMap))
+	for i, m := range strMap {
+		resSort = append(resSort, resStruct{i, m})
+	}
+
+	sort.Slice(resSort, func(i, j int) bool {
+		if resSort[i].value == resSort[j].value {
+			return resSort[i].key < resSort[j].key
+		}
+
+		return resSort[i].value > resSort[j].value
+	})
+
+	for i, k := range resSort {
+		if i > 9 {
+			return res
+		}
+		res = append(res, k.key)
+	}
+
+	return res
 }

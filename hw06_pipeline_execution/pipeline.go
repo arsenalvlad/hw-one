f551ffe809aadabb9b5b4f46671cb16036689f4e
Usov Vlad <vladislav.usov@moex.com>
2023-06-04 11:30:13 +0300

    race not ready

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -14,47 +14,51 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	var wg sync.WaitGroup
+	var sm sync.Mutex
 
 	out := make(Bi)
 	mergeCh := make([]Out, 0, len(in))
 	inin := make(Bi, 1)
 
+	waitCh := make(Bi)
+
 	go func() {
-		<-done
-		close(inin)
-		close(out)
+		select {
+		case <-done:
+			close(out)
+		case <-waitCh:
+			close(out)
+		}
 	}()
 
-	for ko := range in {
-		inin <- ko
-		wg.Add(1)
-		go func() {
-			mergeCh = append(mergeCh, fn1(inin, &done, stages...))
-			wg.Done()
-		}()
-	}
-
 	go func() {
+		for ko := range in {
+			wg.Add(1)
+			inin <- ko
+			go func(sm *sync.Mutex) {
+				sm.Lock()
+				mergeCh = append(mergeCh, fn1(inin, stages...))
+				wg.Done()
+				sm.Unlock()
+			}(&sm)
+		}
+
 		wg.Wait()
+
 		for _, p := range mergeCh {
 			out <- <-p
 		}
-		close(inin)
-		close(out)
+
+		close(waitCh)
 	}()
 
 	return out
 }
 
-func fn1(in In, done *In, stages ...Stage) Out {
-	select {
-	case <-*done:
-		return nil
-	default:
-		if len(stages) == 1 {
-			return stages[0](in)
-		}
-
-		return fn1(stages[0](in), done, stages[1:]...)
+func fn1(in In, stages ...Stage) Out {
+	if len(stages) == 1 {
+		return stages[0](in)
 	}
+
+	return fn1(stages[0](in), stages[1:]...)
 }

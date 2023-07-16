ec2b4eb9f84e179788bcedb0a26b3539df2b4597
Usov Vlad <vladislav.usov@moex.com>
2023-07-16 18:17:37 +0300

    don't work regex[ for a-Z and  \n ((

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +22,81 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
+	result := make(Environment)
+
+	readDir, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, item := range readDir {
+		open, err := os.Open(dir + "/" + item.Name())
+		if err != nil {
+			return nil, fmt.Errorf("could not oepn file: %w", err)
+		}
+
+		defer func(open *os.File) {
+			err := open.Close()
+			if err != nil {
+				fmt.Println(fmt.Errorf("could not close file: %w", err))
+			}
+		}(open)
+
+		info, err := open.Stat()
+		if err != nil {
+			return nil, fmt.Errorf("could not get stat: %w", err)
+		}
+
+		if info.Size() == 0 {
+			result[item.Name()] = EnvValue{
+				Value:      "",
+				NeedRemove: true,
+			}
+			continue
+		}
+
+		res, err := prepareEnv(open)
+		if err != nil {
+			return nil, fmt.Errorf("could not prepare env: %w", err)
+		}
+
+		result[item.Name()] = *res
+	}
 	// Place your code here
-	return nil, nil
+	return result, nil
+}
+
+func prepareEnv(reader io.Reader) (*EnvValue, error) {
+	rd := bufio.NewReader(reader)
+
+	line, err := rd.ReadString('\n')
+	if err != nil && !errors.Is(err, io.EOF) {
+		return nil, fmt.Errorf("could not read string: %w", err)
+	}
+
+	un, err := strconv.Unquote(line)
+	if err != nil {
+		un = line
+	}
+
+	res := strings.ReplaceAll(un, "0x00", string('\n'))
+
+	value := strings.TrimRight(res, " ")
+
+	matchString, err := regexp.MatchString("^[a-zA-Z]+$", value)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return nil, fmt.Errorf("could not match string: %w", err)
+	}
+
+	if !matchString {
+		return &EnvValue{
+			Value:      "",
+			NeedRemove: true,
+		}, nil
+	}
+
+	return &EnvValue{
+		Value:      value,
+		NeedRemove: false,
+	}, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	//dir, err := ReadDir(os.Args[1])
+	dir, err := ReadDir("testdata/env")
+	if err != nil {
+		fmt.Println(fmt.Errorf("could not ge env from dir: %w", err))
+		return
+	}
+
+	code := RunCmd(os.Args[2:], dir)
+	os.Exit(code)
 }

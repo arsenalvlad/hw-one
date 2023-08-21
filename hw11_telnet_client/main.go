74e1c449cbfb11a5efc24250cf49702ac6fe0cc2
Usov Vlad <vladislav.usov@moex.com>
2023-08-21 18:07:57 +0300

    not work

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,100 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", 20, "timeout duration")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	in := &bytes.Buffer{}
+	out := os.Stdout
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	ctx2, stop2 := signal.NotifyContext(ctx, os.Interrupt)
+	defer stop2()
+
+	ctx3, stop3 := signal.NotifyContext(ctx, syscall.SIGQUIT)
+	defer stop3()
+
+	client := NewTelnetClient(os.Args[len(os.Args)-2]+":"+os.Args[len(os.Args)-1], timeout, io.NopCloser(in), out)
+
+	err := client.Connect()
+	if err != nil {
+		fmt.Printf("could not telnet client connect : %s\n", err)
+		return
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go stdinSend(ctx, &wg, in, client)
+
+	go func() {
+		for {
+			select {
+			case <-ctx2.Done():
+				fmt.Println("I\nwill be\nback!")
+				cancel()
+				return
+			case <-ctx3.Done():
+				fmt.Println("I'm telnet client\nBye-bye")
+				cancel()
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
+}
+
+//func recieveWatch(wg *sync.WaitGroup, in bytes.Buffer, t TelnetClient) {
+//	err = t.Receive()
+//	if err != nil {
+//		fmt.Printf("could not receive message: %s\n", err)
+//		wg.Done()
+//		return
+//	}
+//}
+
+func stdinSend(ctx context.Context, wg *sync.WaitGroup, in *bytes.Buffer, t TelnetClient) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		select {
+		case <-ctx.Done():
+			wg.Done()
+			return
+		default:
+			fmt.Println(scanner.Text(), "one")
+			in.WriteString(scanner.Text())
+			err := t.Send()
+			if err != nil {
+				fmt.Printf("could not send message: %s\n", err)
+				wg.Done()
+				return
+			}
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("scanner Err: %s\n", err)
+			}
+		}
+
+	}
+
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,62 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type TelnetClienter struct {
+	Address string
+	Timeout time.Duration
+	In      io.ReadCloser
+	Out     io.Writer
+	Conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &TelnetClienter{
+		Address: address,
+		Timeout: timeout,
+		In:      in,
+		Out:     out,
+		Conn:    nil,
+	}
+}
+
+func (t *TelnetClienter) Connect() error {
+	conn, err := net.Dial("tcp", t.Address)
+	if err != nil {
+		return fmt.Errorf("can not connect: %w", err)
+	}
+
+	t.Conn = conn
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *TelnetClienter) Send() error {
+	sms, err := io.ReadAll(t.In)
+	if err != nil {
+		return fmt.Errorf("can not read all from stdin: %w", err)
+	}
+	_, err = t.Conn.Write(sms)
+	if err != nil {
+		return fmt.Errorf("can not write message to host: %w", err)
+	}
+
+	return nil
+}
+
+func (t *TelnetClienter) Receive() error {
+	_, err := io.Copy(t.Out, t.Conn)
+	if err != nil {
+		return fmt.Errorf("can not receive message to stdout: %w", err)
+	}
+
+	return nil
+}
+
+func (t *TelnetClienter) Close() error {
+	err := t.Conn.Close()
+	if err != nil {
+		return fmt.Errorf("can not close connect: %w", err)
+	}
+
+	return nil
+}
